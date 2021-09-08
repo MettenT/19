4b0eae89fec0b249667dcc7a2c5aa5abbe467923
tmetten <tmetten@student.s19.be>
2021-09-08 20:22:51 +0000

    ex02

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -10,4 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		if (('a' <= str[i] && str[i] <= 'z') || ('A' <= str[i] && str[i] <= 'Z'))
+		{
+			i++;
+		}
+		else 
+		{
+			return (0);
+		}
+	}	
+	return(1);
+}
