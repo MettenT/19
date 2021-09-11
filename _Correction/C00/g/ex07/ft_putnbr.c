7b9b06ca4a7a40b64e4323abd35cf42aca4ea0c8
tmetten <tmetten@student.s19.be>
2021-09-11 17:56:47 +0000

    mm

--- _Correction/C00/g/ex07/ft_putnbr.c
+++ _Correction/C00/g/ex07/ft_putnbr.c
@@ -1,44 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ex07.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmetten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: gscarama <gscarama@student.s19.be>			+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/06 12:06:11 by tmetten           #+#    #+#             */
-/*   Updated: 2021/09/06 12:06:12 by tmetten          ###   ########.fr       */
+/*   Created: 2021/09/06 12:22:42 by gscarama          #+#    #+#             */
+/*   Updated: 2021/09/09 12:24:45 by gscarama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char a)
+void	ft_putchar(char c)
 {
-	write(1, &a, 1);
+	write(1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
 {
-	if (nb == -2147483648)
-	{
-		ft_putchar('-');
-		ft_putchar('2');
-		nb = 147483648;
-	}
+	unsigned int		a;
+
 	if (nb < 0)
 	{
 		ft_putchar('-');
-		nb *= -1;
-	}	
-	if (nb < 10)
-	{
-		ft_putchar(nb + 48);
-		return;
+		a = -nb;
 	}
 	else
 	{
-		ft_putnbr(nb / 10);		
+		a = nb;
 	}
-	ft_putnbr(nb % 10);
+	if (a >= 10)
+	{
+		ft_putnbr(a / 10);
+		a %= 10;
+	}
+	ft_putchar(a + '0');
 }
 
+int	main()
+{
+	ft_putnbr(0);
+}
