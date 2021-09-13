8401630196da0ae70f006df7828c1f860452f6a2
tmetten <tmetten@student.s19.be>
2021-09-13 08:13:21 +0000

    C02

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -14,10 +14,10 @@
 
 unsigned int ft_strlcpy( char *dest, char *src, unsigned int size)
 {
-	int i;
+	unsigned int i;
 
 	i = 0;
-	while(src[i] != '\0')
+	while(src[i] != '\0' && i < size - 1)
 	{
 		dest[i] = src[i];
 		i++;		
