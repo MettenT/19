8ee509c70857ffc908bd335ee97141101027bafa
tmetten <tmetten@student.s19.be>
2021-09-14 21:25:37 +0000

    C06

--- C06/ex01/ft_print_params.c
+++ C06/ex01/ft_print_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmetten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/14 20:23:43 by tmetten           #+#    #+#             */
-/*   Updated: 2021/09/14 20:23:44 by tmetten          ###   ########.fr       */
+/*   Created: 2021/09/14 21:00:00 by tmetten           #+#    #+#             */
+/*   Updated: 2021/09/14 21:00:01 by tmetten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,25 @@ void	ft_putchar(char a)
 	write(1, &a, 1);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
 	int i;
+	int j;
 
-	i = 0;
-	if (argc > 0)
+	i = 1;
+	if (argc > 1)
 	{
-		while (argv[0][i] != '\0')
+		while (i < argc)
 		{
-			ft_putchar(argv[0][i]);
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				ft_putchar(argv[i][j]);
+				j++;
+			}
+			ft_putchar('\n');
 			i++;
 		}
-	}	
-	ft_putchar('\n');
+	}
 	return(0);
 }
--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmetten <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/14 20:23:43 by tmetten           #+#    #+#             */
-/*   Updated: 2021/09/14 20:23:44 by tmetten          ###   ########.fr       */
+/*   Created: 2021/09/14 21:18:05 by tmetten           #+#    #+#             */
+/*   Updated: 2021/09/14 21:18:09 by tmetten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,25 @@ void	ft_putchar(char a)
 	write(1, &a, 1);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	int i;
+	int count;
+	int j;
 
-	i = 0;
-	if (argc > 0)
+	count = argc - 1;
+	if (argc > 1)
 	{
-		while (argv[0][i] != '\0')
+		while (count >= 1)
 		{
-			ft_putchar(argv[0][i]);
-			i++;
+			j = 0;
+			while (argv[count][j] != '\0')
+			{
+				ft_putchar(argv[count][j]);
+				j++;
+			}
+			ft_putchar('\n');
+			count--;
 		}
-	}	
-	ft_putchar('\n');
+	}
 	return(0);
 }
