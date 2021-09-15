c959698865204c7113f84d517d55952f546cd6fb
tmetten <tmetten@student.s19.be>
2021-09-15 16:27:26 +0000

    norm

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -10,27 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-
-int ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
+	unsigned int	i;
 
 	i = 0;
 	while (i < n - 1 && s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
-		i++;		
+		i++;
 	}
 	return (s1[i] - s2[i]);
 }
 
-int		main()
-{
-	char s1[] = "Empty";
-	char s2[] = "Testing comparison";
-	char s3[] = "Test";
+// #include <stdio.h>
+// #include <string.h>
+// int		main()
+// {
+// 	char s1[] = "Empty";
+// 	char s2[] = "Testing comparison";
+// 	char s3[] = "Test";
 
-	printf("s12: %d, s21: %d, s13: %d, s31: %d, s23: %d, s32: %d", ft_strncmp(s1, s2, 5), ft_strncmp(s2, s1, 5), ft_strncmp(s1, s3, 5), ft_strncmp(s3, s1, 5), ft_strncmp(s2, s3, 1), ft_strncmp(s3, s2, 5));
-	printf("\n\ns12: %d, s21: %d, s13: %d, s31: %d, s23: %d, s32: %d", strncmp(s1, s2, 5), strncmp(s2, s1, 5), strncmp(s1, s3, 5), strncmp(s3, s1, 5), strncmp(s2, s3, 1), strncmp(s3, s2, 5));
-}
+// 	printf("s12: %d, s21: %d, ", ft_strncmp(s1, s2, 5), ft_strncmp(s2, s1, 5));
+// 	printf("s13: %d, s31: %d, ", ft_strncmp(s1, s3, 5), ft_strncmp(s3, s1, 5));
+// 	printf("s23: %d, s32: %d\n", ft_strncmp(s2, s3, 1), ft_strncmp(s3, s2, 5));
+// 	printf("\ns12: %d, s21: %d, ", strncmp(s1, s2, 5), strncmp(s2, s1, 5));
+// 	printf("s13: %d, s31: %d, ", strncmp(s1, s3, 5), strncmp(s3, s1, 5));
+// 	printf("s23: %d, s32: %d", strncmp(s2, s3, 1), strncmp(s3, s2, 5));
+// }
+
+// DESCRIPTION
+// 	The strcmp() function compares the two strings s1 and s2.  
+// 	It returns an integer less than, equal to, or greater than 
+// 	zero if s1 is found, respectively, to be less than, to match,
+// 	or be greater than s2.
+
+// 	The strncmp() function is similar, except it compares only 
+// 	the first (at most) n bytes of s1 and s2.
+
+// RETURN VALUE
+// 	The strcmp() and strncmp() functions return an integer less 
+// 	than, equal to, or greater than zero if s1 (or the first n 
+// 	bytes thereof) is found, respectively, to be less  than,  to
+// 	match, or be greater than s2.
